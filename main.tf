848b54116c825ca6fdfa53a236f122b506ed1027
daler-kelajak <m.proud78@gmail.com>
2022-03-30 18:32:52 -0500

    test terraform runners

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+###############        AWS Auth        #################
+
+locals {
+  kustomization = <<KUSTOMIZATION
+apiVersion: v1/batch
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: somearn
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+KUSTOMIZATION
+}
+
+resource "local_file" "foo" {
+  content  = local.kustomization
+  filename = "kustomization.yaml"
+  provisioner "local-exec" {
+    command = "git add . && git commit -m 'pushed from terraform' && git push origin -u main  && rm ./kustomization.yaml"
+    interpreter = []
+  }
+}
