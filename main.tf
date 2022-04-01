d6e9e49ae0f0a6ea34d7ff20be128deb8ea5f2b4
daler-kelajak <m.proud78@gmail.com>
2022-03-31 22:55:00 -0500

    wonderfull commit from me

--- main.tf
+++ main.tf
@@ -21,8 +21,26 @@ KUSTOMIZATION
 resource "local_file" "foo" {
   content  = local.kustomization
   filename = "kustomization.yaml"
-  # provisioner "local-exec" {
-  #   command     = " git config --global user.email m.proud78.com && git config --global user.name kebzur && git add . && git commit -m 'pushed from terraform' && git push origin -u main -f"
-  #   interpreter = []
-  # }
+}
+
+locals {
+  es-napshot = <<KUSTOMIZATION
+apiVersion: v1/batch
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: somearn
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+KUSTOMIZATION
+}
+
+resource "local_file" "es-napshot" {
+  content  = local.es-napshot
+  filename = "./es-snapshot/es-napshot-kustomization.yaml"
 }
